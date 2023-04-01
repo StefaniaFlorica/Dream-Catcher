d97906c339eab9d79ba2db675ff9ac6665ed4386
StefaniaFlorica <floricastefania@yahoo.com>
2023-04-01 20:25:48 +0300

    fix chart error

--- frontend/dream-catch/src/components/Reports.js
+++ frontend/dream-catch/src/components/Reports.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Header from "./Header";
 import SleepMetricsService from "../services/SleepMetricsService";
 import {
@@ -6,15 +6,18 @@ import {
   InputLabel,
   MenuItem,
   Select,
-  FormHelperText,
   Grid,
   Button,
 } from "@mui/material";
-import BarChart from "./BarChart";
 import { Bar } from "react-chartjs-2";
+import "chart.js/auto"; // ADD THIS
 
 function Reports() {
+  const ref = useRef();
+
+  const sleepMetricsService = new SleepMetricsService();
   const [type, setType] = useState("");
+  const [frequency, setFrequency] = useState("");
   const [isShown, setIsShown] = useState(false);
   const [data, setData] = useState(null);
   const [chartData, setChartData] = useState({
@@ -30,56 +33,45 @@ function Reports() {
   });
 
   const reportTypes = ["duration", "energy", "stress"];
+  const frequencyTypes = ["weekly", "monthly"];
 
-  const handleChange = (event) => {
+  const handleChangeType = (event) => {
     setType(event.target.value);
   };
+  const handleChangeFrequency = (event) => {
+    setFrequency(event.target.value);
+  };
 
-    useEffect(() => {
-        if(data!=null)
-        {
-            console.log("data has changed: ", chartData);
+  useEffect(() => {
+    if (data != null) {
+      // console.log("data has changed: ", chartData);
       setChartData({
-          labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-          datasets: [
-            {
-              label: type,
-              data: Object.values(data),
-              borderColor: "black",
-              borderWidth: 2,
-            },
-          ],
-        });
-        setIsShown(true);
-        }
-      
-    }, [data]);
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: type,
+            data: Object.values(data),
+            borderColor: "black",
+            borderWidth: 2,
+          },
+        ],
+      });
+      setIsShown(true);
+    }
+  }, [data, type, frequency]);
 
   const handleButtonClick = (event) => {
-    if(type==="")
-    {
-        alert("Please select a report type!");
-        return
+    if (type === "" || frequency === "") {
+      alert("Please pick a choice for each category!");
+      return;
     }
-    SleepMetricsService.generateReport(type)
+    sleepMetricsService
+      .generateReport(frequency, type)
       .then((res) => res.data)
       .then((data1) => {
         setData(data1);
-        // setChartData({
-        //             labels: ['1','2','3','4','5','6','7'],
-        //             datasets: [
-        //               {
-        //                 label: type,
-        //                 data: Object.values(data),
-        //                 borderColor: "black",
-        //                 borderWidth: 2,
-        //               },
-        //             ],
-        //           });
-        
-      });
-      
-    // 
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -88,10 +80,11 @@ function Reports() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        flexDirection: "column",
         padding: 20,
       }}
     >
-      <Grid justify="center" alignItems="center" direction="column">
+      <Grid justify="center" alignItems="center">
         <Header text="Generate report" />
         <FormControl sx={{ m: 1, minWidth: 120 }}>
           <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
@@ -100,7 +93,7 @@ function Reports() {
             id="demo-simple-select-helper"
             value={type}
             label="Type"
-            onChange={handleChange}
+            onChange={handleChangeType}
             style={{ width: 300 }}
           >
             <MenuItem value="">
@@ -112,18 +105,45 @@ function Reports() {
               </MenuItem>
             ))}
           </Select>
-          <FormHelperText>Select only ONE category</FormHelperText>
         </FormControl>
-        <div>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-helper-label2">
+            Frequency
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label2"
+            id="demo-simple-select-helper2"
+            value={frequency}
+            label="Frequency"
+            onChange={handleChangeFrequency}
+            style={{ width: 300 }}
+          >
+            <MenuItem value="">
+              <em></em>
+            </MenuItem>
+            {frequencyTypes.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
           <Button
             variant="contained"
-            style={{ marginLeft: 100 }}
+            style={{ marginLeft: 250 }}
             onClick={handleButtonClick}
           >
             Generate
           </Button>
         </div>
-        <div>{isShown ? <Bar data={chartData} /> : <p></p>}</div>
+        <div>{isShown ? <Bar ref={ref} data={chartData} /> : <p></p>}</div>
       </Grid>
     </div>
   );
--- frontend/dream-catch/src/services/SleepMetricsService.js
+++ frontend/dream-catch/src/services/SleepMetricsService.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 const METRICS_API_BASE_URL = "http://localhost:8080/api/metricsService";
 class SleepMetricsService {
-  generateReport(recordType) {
-    return axios.get(METRICS_API_BASE_URL + "/getReport/" + recordType);
+  generateReport(frequencyType, recordType) {
+    return axios.get(
+      METRICS_API_BASE_URL + "/getReport/" + frequencyType + "/" + recordType
+    );
   }
 }
-export default new SleepMetricsService();
+export default SleepMetricsService;
